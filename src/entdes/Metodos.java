a2470b1df2f38e7f618e9da7ce99aeef93e53d76
Jesus Murciano <jesusmurcianob@gmail.com>
2021-03-03 10:13:35 +0100

    Metodos concatenar strings

--- src/entdes/Metodos.java
+++ src/entdes/Metodos.java
@@ -1,11 +1,28 @@
 package entdes;
 
+import java.util.Scanner;
+
 public class Metodos {
 
 	public static void main(String[] args) {
 		
 		
-		
+		devuelveStringsContatenados();
 	}
 
+	static void devuelveStringsContatenados() {
+		
+		Scanner scanner =new Scanner(System.in);
+	
+			
+		System.out.println("Introduzca su primera cadena: ");
+		String cad1=scanner.nextLine();
+		
+		
+		System.out.println("Introduzca su primera cadena: ");
+		String cad2=scanner.nextLine();
+		
+		System.out.println("Su texto es: "+cad1+cad2);
+		
+	}
 }
